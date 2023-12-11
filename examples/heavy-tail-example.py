bf7670e2fa0e5fa72147834d8933413f3d7e7ce2
Mostafa Farrag <moah.farag@gmail.com>
2023-12-11 20:31:41 +0100

    update examples

--- examples/heavy-tail-example.py
+++ examples/heavy-tail-example.py
@@ -1,27 +1,32 @@
+"""Heavy tail example."""
 import pandas as pd
 
-rdir = rf"\\MYCLOUDEX2ULTRA\research\phd\heavy-tail-statistics"
-from statista.distributions import GEV
+rdir = rf"examples/data"
+from statista.distributions import GEV, Distributions
 
-#%%
-dung = pd.read_csv(f"{rdir}/dung/pdf_obs.txt", delimiter=" ")
+# %%
+dung = pd.read_csv(f"{rdir}/pdf_obs.txt", delimiter=" ")
 dung.sort_values(by="ams", inplace=True)
+
 scale = 2132.938715
 loc = 6582.059315
 shape = 0.0486556
+parameters = {
+    "loc": loc,
+    "scale": scale,
+    "shape": shape,
+}
 ams = dung["ams"].values
 
-
-dist_positive = GEV(ams, shape, loc, scale)
-pdf = dist_positive.pdf(shape, loc, scale, plot_figure=False)
+gev_positive = Distributions("GEV", ams, parameters)
+pdf = gev_positive.pdf(parameters, plot_figure=False)
 dung["scipy +ve"] = pdf
 
-shape = -0.0486556
-dist_negative = GEV(ams, shape, loc, scale)
-pdf = dist_negative.pdf(shape, loc, scale, plot_figure=False)
+parameters["shape"] = -0.0486556
+dist_negative = Distributions("GEV", ams, parameters)
+pdf = dist_negative.pdf(parameters, plot_figure=False)
 dung["scipy -ve"] = pdf
-#%%
+# %%
 method = "lmoments"  # "mle"
 parameters_lm = dist_negative.fit_model(method=method)
 parameters_mle = dist_negative.fit_model(method="mle")
-#%%
