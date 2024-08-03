792764e9f547981b1302503c9a8ded507a8ec8a7
Mostafa Farrag <moah.farag@gmail.com>
2024-08-03 23:28:37 +0200

    try to separate the two line equation

--- statista/distributions.py
+++ statista/distributions.py
@@ -1001,6 +1001,7 @@ class GEV(AbstractDistribution):
             \\begin{cases}
                 \\left( 1+ \\xi \\left(\\frac{x-\\zeta}{\\delta} \\right) \\right)^\\frac{-1}{\\xi}  & \\quad
                 \\land\\xi\\neq 0 \\
+
                 e^{- \\left(\\frac{x-\\zeta}{\\delta} \\right)} & \\quad   \\land \\xi=0
             \\end{cases}
         \\]
